46e848e01d7ba5b5dd749e166a3ce9b2abde5c0e
ivanhrabcak <ivan.hrabcak@gmail.com>
2020-05-16 23:05:04 +0200

    fix variable storing huffmanCoding.getTree() in Main

--- src/main/java/me/ivik/huffmanalgorithm/Main.java
+++ src/main/java/me/ivik/huffmanalgorithm/Main.java
@@ -2,11 +2,12 @@ package me.ivik.huffmanalgorithm;
 
 import me.ivik.huffmanalgorithm.tree.CharacterOrBranch;
 import me.ivik.huffmanalgorithm.tree.MyNumber;
+import me.ivik.huffmanalgorithm.tree.Tree;
 
 public class Main {
     public static void main(String[] args) {
         HuffmanCoding huffmanCoding = new HuffmanCoding("aaababbzzccaaazzbbbz");
-        DoubleEndedPriorityQueue<CharacterOrBranch> doubleEndedPriorityQueue = huffmanCoding.getTree();
+        Tree doubleEndedPriorityQueue = huffmanCoding.getTree();
 
     }
 }
